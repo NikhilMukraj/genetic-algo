85ddeac2c0efef387a7f75557b6df79355c9f6a1
Nikhiljag <nikhilmukraj@gmail.com>
2022-06-23 16:28:56 -0400

    started work on wrapper

--- wrapper.py
+++ wrapper.py
@@ -1,1 +1,8 @@
 import ctypes
+
+
+def toCDoubleArray(arr):
+    return (ctypes.c_double * len(arr))(*arr)
+
+def toPyArray(arr, length):
+    return ctypes.cast(length, ctypes.POINTER(ctypes.c_int * length)).contents # need to test
