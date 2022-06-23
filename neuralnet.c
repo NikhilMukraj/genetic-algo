aa4b3d52b7ece616858386226af92606e010ebff
Nikhiljag <nikhilmukraj@gmail.com>
2022-06-23 19:01:47 -0400

    network init function

--- neuralnet.c
+++ neuralnet.c
@@ -8,7 +8,16 @@ struct nn {
     int len;
 }; // array of layers
 
-double *feedforward(struct nn network, double *inputs) {
+void network_init(struct nn *network, int randomized) {
+    if (randomized == 1) {
+        // generate network with weights and biases
+        // set to random numbers between -1 and 1
+    } else {
+        // generate network with weights and biases of 0.0
+    }
+}
+
+double *feedforward(struct nn *network, double *inputs) {
     // get prediction based on inputs
     return;
 }
