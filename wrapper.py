b0742defbda95ff43a95431efc60ea32db4e2bc0
Nikhiljag <nikhilmukraj@gmail.com>
2022-06-30 23:18:19 -0400

    added stuff to wrapper

--- wrapper.py
+++ wrapper.py
@@ -6,3 +6,6 @@ def toCDoubleArray(arr):
 
 def toPyArray(arr, length):
     return ctypes.cast(length, ctypes.POINTER(ctypes.c_int * length)).contents # need to test
+
+def getNetworkStruct(args):
+    pass
