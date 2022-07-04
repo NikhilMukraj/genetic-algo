f43c6714e6fbfba2466be25573e6b563bc186bf6
Nikhiljag <nikhilmukraj@gmail.com>
2022-07-04 19:45:52 -0400

    added more tests

--- tests.py
+++ tests.py
@@ -2,5 +2,11 @@ import wrapper as w
 
 
 # gcc -fPIC -shared -rdynamic neuralnet.c -o neuralnet.so
-c_func = w.init_c()
-c_func.a_tanh()
+lib = w.init_c()
+
+inputs = w.toCDoubleArray([0, 0])
+weights = w.toCDoubleArray([0, 0])
+bias = w.toCDouble(bias)
+length = w.toCInt(length)
+
+print(lib.sigmoid(inputs, weights, bias, length))
