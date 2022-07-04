e54264e44341f54ebdf6d27994139c1435aa2583
Nikhiljag <nikhilmukraj@gmail.com>
2022-07-03 22:21:20 -0400

    layerstruct

--- wrapper.py
+++ wrapper.py
@@ -9,8 +9,13 @@ def init_c():
 
 #https://stackoverflow.com/questions/24640817/python-ctypes-definition-for-c-struct
 #https://doc.sagemath.org/html/en/thematic_tutorials/numerical_sage/ctypes_examples.html
+#https://github.com/jima80525/ctypes_example/blob/master/tutorial2/testWrappedPoint.py
 class LayerStruct(ctypes.Structure):
-    _fields_ = []
+    _fields_ = [('input_len', ctypes.c_int),
+                ('layer_len', ctypes.c_int),
+                ('weights', ctypes.POINTER(ctypes.POINTER(ctypes.c_double))),
+                ('biases', ctypes.POINTER(ctypes.c_double)),
+                ('activation', ctypes.c_char)]
 
 class NNStruct(ctypes.Structure):
     _fields_ = []
