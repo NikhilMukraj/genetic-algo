efb8a32211ef36a5759f87ec4f5c8e669997ed5d
Nikhiljag <nikhilmukraj@gmail.com>
2022-07-05 19:01:52 -0400

    worked out how to convert functions

--- tests.py
+++ tests.py
@@ -1,13 +1,19 @@
 import wrapper as w
+import ctypes as c
 
 
 # gcc -fPIC -shared -rdynamic neuralnet.c -o neuralnet.so
 lib = w.init_c()
 
-inputs = w.toCDoubleArray([0, 0])
-weights = w.toCDoubleArray([0, 0])
-bias = w.toCDouble(bias)
-length = w.toCInt(length)
+inputs = w.toCDoubleArray([1])
+weights = w.toCDoubleArray([1])
+bias = w.toCDouble(0)
+length = w.toCInt(1)
 
-answer = lib.sigmoid(inputs, weights, bias, length)
-print(w.CDoubletoPyArray(answer, length))
+sig = lib.a_sigmoid
+
+sig.argtype = [c.POINTER(c.c_double), c.POINTER(c.c_double), c.c_double, c.c_int]
+sig.restype = c.c_double
+
+answer = sig(inputs, weights, bias, length)
+print(answer)
