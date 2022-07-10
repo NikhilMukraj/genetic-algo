7d365d7a6f5a14537064623a44956ff658e4a041
Nikhiljag <nikhilmukraj@gmail.com>
2022-07-10 19:16:46 -0400

    keeping naming consistent throughout langs

--- wrapper.py
+++ wrapper.py
@@ -18,14 +18,14 @@ def init_c():
 #https://stackoverflow.com/questions/24640817/python-ctypes-definition-for-c-struct
 #https://doc.sagemath.org/html/en/thematic_tutorials/numerical_sage/ctypes_examples.html
 #https://github.com/jima80525/ctypes_example/blob/master/tutorial2/testWrappedPoint.py
-class LayerStruct(ctypes.Structure):
+class layer(ctypes.Structure):
     _fields_ = [('input_len', ctypes.c_int),
                 ('layer_len', ctypes.c_int),
                 ('weights', ctypes.POINTER(ctypes.POINTER(ctypes.c_double))),
                 ('biases', ctypes.POINTER(ctypes.c_double)),
                 ('activation', ctypes.c_char)]
 
-class NNStruct(ctypes.Structure):
+class nn(ctypes.Structure):
     _fields_ = [('layers', ctypes.POINTER(LayerStruct)),
                 ('len', ctypes.c_int)]
 
