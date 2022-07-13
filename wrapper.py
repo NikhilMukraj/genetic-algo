3699a2340b4a56ea8730f8b8d7c9b5c34b5ed605
Nikhiljag <nikhilmukraj@gmail.com>
2022-07-13 16:55:52 -0400

    weights arg test

--- wrapper.py
+++ wrapper.py
@@ -81,6 +81,8 @@ def writeToLayer(layer, arr, shape):
         raise ValueError(f'Weights with shape of {shape}')
 
 def init_weights(lib, layer, randomized):
+    if randomized not in [0, 1]:
+        raise ValueError(f'{randomized} is not a valid argument')
     lib.init_weights(ctypes.POINTER(layer), toCInt(randomized))
 
 def init_bias(lib, layer, randomized):
